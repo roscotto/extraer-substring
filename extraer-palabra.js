f3c59e205a4f066c937f7bbb8ed1eb96508cb31d
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2022-12-11 20:41:58 -0300

    subida de archivos

---
+++ extraer-palabra.js
@@ -0,0 +1,9 @@
+//Algoritmo de extracción de palabra en una oración con Javascript. (Extraer un subtring)
+
+let frase = 'Las flores amarillas';
+let initial = 4;
+let final = 10;
+
+palabra = frase.substring(initial,final);
+
+console.log(palabra);
---
+++ main.py
@@ -0,0 +1,15 @@
+#Algoritmo de extracción de palabras en una oración en Python. (Extraer un subtring)
+
+frase = 'las flores amarillas'
+
+#definimos punto de inicio y punto final. posicion de la primera y ultima letra
+
+inicial = 4
+final = 10
+
+#donde almacenaremos lo que extraemos
+
+palabra = frase[inicial:final]
+
+print(palabra)
+
